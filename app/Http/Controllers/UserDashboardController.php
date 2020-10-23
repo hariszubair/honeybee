ac830fd45177964b781a44bd4a975d7919071242
Muhammad Haris <haris.zubair@outlook.com>
2020-10-23 17:55:22 +0500

    profile update status

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -288,7 +288,7 @@ class UserDashboardController extends Controller
           }
           else{
         Session::flash('success', 'Profile Update');
-            return redirect()->route('profile');
+            return redirect('profile');
           }     
          
     }
